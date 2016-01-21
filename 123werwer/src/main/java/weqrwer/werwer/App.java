1903599b1f7f464b034ab86992bf4222ca9f0f8b
Kenny <Kenny@Kenny-PC.mshome.net>
2016-01-21 23:31:59 +0800

    123

---
+++ 123werwer/src/main/java/weqrwer/werwer/App.java
@@ -0,0 +1,13 @@
+package weqrwer.werwer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
