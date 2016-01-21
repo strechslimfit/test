ecb89b9d05fbfc642a1bf24cdda0550cd180f0e2
Kenny <Kenny@Kenny-PC.mshome.net>
2016-01-21 23:25:51 +0800

    wer

---
+++ ewr/src/main/java/weqrwer/ewr/App.java
@@ -0,0 +1,13 @@
+package weqrwer.ewr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
